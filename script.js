6b5e2946456354cce746330b1aecbad82adc196c
Shorya <139441120+121ShoryaSingh@users.noreply.github.com>
2025-10-30 01:13:20 +0530

    created a submission

--- script.js
+++ script.js
@@ -4,7 +4,8 @@ function Person(name, age) {
 }
 
 Person.prototype.greet = function() {
-  console.log(`Hello, my name is ${this.name}, I am ${this.age} years old.`);
+  // Cypress expects: "Hello, my name is Alice and I am 25 years old."
+  console.log(`Hello, my name is ${this.name} and I am ${this.age} years old.`);
 };
 
 function Employee(name, age, jobTitle) {
@@ -15,6 +16,7 @@ function Employee(name, age, jobTitle) {
 Employee.prototype = Object.create(Person.prototype);
 Employee.prototype.constructor = Employee;
 
+// Cypress expects jobGreet to include the comma before "my job title", but "and I am ..."
 Employee.prototype.jobGreet = function() {
   console.log(`Hello, my name is ${this.name}, I am ${this.age} years old, and my job title is ${this.jobTitle}.`);
 };
